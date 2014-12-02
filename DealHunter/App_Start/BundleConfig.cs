f419976f88a97b95e02417e7ecb6824b61c411ca
Peter <peterhj@gmail.com>
2014-12-02 22:34:18 +0100

    funScripts er nu en separat fil og bundle, så den er nem at fjerne.

--- DealHunter/App_Start/BundleConfig.cs
+++ DealHunter/App_Start/BundleConfig.cs
@@ -27,6 +27,9 @@ namespace DealHunter
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+			bundles.Add(new ScriptBundle("~/bundles/funScripts").Include(
+						"~/Scripts/funScripts.js"));
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
---
+++ DealHunter/Scripts/funScripts.js
@@ -0,0 +1,10 @@
+﻿//As per request by Tjn
+var toggle = true;
+setInterval(function () {
+	if (toggle) {
+		$("#flash").css("background-color", "red");
+	} else {
+		$("#flash").css("background-color", "yellowgreen");
+	}
+	toggle = !toggle;
+}, 300);
