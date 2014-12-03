e4c331b7ce07894fc62ac61ef567dcd93e24acef
Pehj <peterhj@gmail.com>
2014-12-03 17:42:52 +0100

    Eksempel på hvordan man kan loade en side ind i en iframe. Jeg får dog ikke lov til at bruge JQuery til at søge igennem det. Tror det kræver vi læser html ned i controlleren, og får den til at poste siden til os, og så kan vi så sætte det ind i en iframe og søge på det.

--- DealHunter/Scripts/funScripts.js
+++ DealHunter/Scripts/funScripts.js
@@ -1,10 +1,26 @@
-﻿//As per request by Tjn
-var toggle = true;
-setInterval(function () {
-	if (toggle) {
-		$("#flash").css("background-color", "red");
-	} else {
-		$("#flash").css("background-color", "yellowgreen");
-	}
-	toggle = !toggle;
-}, 300);
+﻿//Shorthand for $( document ).ready()
+$(function() {
+	//Iframe
+	$("#iframeBtn").click(function () {
+		console.log("inside click iframe");
+		var userUrl = $("#iframeUrl").val();
+		console.log(userUrl);
+		$("#iframe").attr("src", userUrl);
+	});
+
+	$("#findPriceBtn").click(function() {
+		var price = $("#iframe").contents().find("span[itemprop=price]").val().trim();
+		$("#foundPriceInput").val(price);
+	});
+
+//As per request by Tjn
+	var toggle = true;
+	setInterval(function() {
+		if (toggle) {
+			$("#flash").css("background-color", "red");
+		} else {
+			$("#flash").css("background-color", "yellowgreen");
+		}
+		toggle = !toggle;
+	}, 300);
+});
